59d83d872c1f7ccf650e0f5d1575082cc1ef740a
Subhrodev <subhrodev.info@gmail.com>
2026-08-12 00:21:23 +0530

    Db fetch call

--- RestAPIs/src/main/java/com/code/project/fresh/RestAPIs/controller/StudentController.java
+++ RestAPIs/src/main/java/com/code/project/fresh/RestAPIs/controller/StudentController.java
@@ -1,15 +1,26 @@
 package com.code.project.fresh.RestAPIs.controller;
 
+import java.util.List;
 
-import com.code.project.fresh.RestAPIs.dto.StudentDto;
+import com.code.project.fresh.RestAPIs.entity.Student;
+import com.code.project.fresh.RestAPIs.repository.StudentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
+
     @GetMapping("/student")
-    public StudentDto getStudent(){
-        return new StudentDto(21,"Subhrodev","subhrodebb@gmail.com");
+    public List<Student> getStudent(){
+        for(Student student: studentRepository.findAll() )      System.out.println(student.toString());
+        return studentRepository.findAll();
     }
 }
